9f28b61c34bab7667d334ec0c740cde4166145dc
Franco Rezabek <rezabekf@amazon.co.uk>
2019-04-02 11:34:57 +0100

    Add get latest AMZ2 Linux AMIs

--- get_latest_amz2_amis.sh
+++ get_latest_amz2_amis.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# get the all AWS regions
+# get all the AWS regions
 regions=$(aws ec2 describe-regions --query Regions[].RegionName | jq '.[]' -r)
 
-# go through all regions and grab AMI
+# go through all the AWS regions and grab Amazon Linux 2 AMIs for them
 while read -r line; do
     printf "$line\n$(aws ssm get-parameters --names \
         /aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2 \
